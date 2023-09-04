b7df5c3dec6216fcd50144d6b502703789670cdf
omaomach <aaronjoash44@gmail.com>
2023-09-04 03:26:43 +0300

    created app layout and model class

---
+++ app/src/main/java/com/omao/worldcupapplication/CountryModel.kt
@@ -0,0 +1,14 @@
+package com.omao.worldcupapplication
+
+class CountryModel {
+
+    var name: String = ""
+    var cupWins: String = ""
+    var flagImage: Int = 0
+
+    constructor(name: String, cupWins: String, flagImage: Int) {
+        this.name = name
+        this.cupWins = cupWins
+        this.flagImage = flagImage
+    }
+}
---
+++ app/src/main/java/com/omao/worldcupapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.omao.worldcupapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
